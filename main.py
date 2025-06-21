230a5f4005498059bd00564e40525f3ff0c2f83f
To-Rex <torex.amaki@gmail.com>
2025-06-21 22:01:26 +0500

    commit

--- main.py
+++ main.py
@@ -75,6 +75,6 @@ app.add_middleware(
 app.include_router(router)
 
 if __name__ == "__main__":
-    #uvicorn.run(app, host="0.0.0.0", port=8001)
-    uvicorn.run(app, port=8001)
+    uvicorn.run(app, host="0.0.0.0", port=8001)
+    #.run(app, port=8001)
     #uvicorn.run(app)
